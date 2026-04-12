4db59679acdcd29790e763d64e42d9932cb4fd5c
MartínOis <martinoisro@gmail.com>
2026-04-12 19:11:46 +0000

    Backup: 2026-04-12 19:11:46

--- backup.sh
+++ backup.sh
@@ -1,7 +1,6 @@
 cd /mnt/bigdata
 
-git remote set-url origin https://MAOIRO17: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@github.com/MAOIRO17/backup_proy_final.git
-
+git remote set-url origin https://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@github.com/MAOIRO17/backup_proy_final.git
 git config --global core.quotepath false
 git add .
 git commit -m "Backup: $(date '+%Y-%m-%d %H:%M:%S')"
