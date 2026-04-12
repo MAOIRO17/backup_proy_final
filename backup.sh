d532c6ffcffc1765dd1240863a3af0c2ccf6054b
MartínOis <martinoisro@gmail.com>
2026-04-12 19:24:41 +0000

    Backup automático: 2026-04-12 19:24:41

--- backup.sh
+++ backup.sh
@@ -1,8 +1,16 @@
+#!/bin/bash
+
+# 1. Cargar el entorno de Python (Conda)
+export PATH="/mnt/bigdata/miniconda/bin:$PATH"
 source /mnt/bigdata/miniconda/etc/profile.d/conda.sh
 conda activate scraping
 
+# 2. Ir a la carpeta del proyecto
 cd /mnt/bigdata
+
+# 3. Subir los cambios a GitHub
+# No hace falta poner el token aquí, ya lo guardamos en el 'git remote'
 git config --global core.quotepath false
 git add .
-git commit -m "Backup: $(date '+%Y-%m-%d %H:%M:%S')"
+git commit -m "Backup automático: $(date '+%Y-%m-%d %H:%M:%S')"
 git push origin main
